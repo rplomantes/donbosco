fb66c4d1de8655fce845098fb69723d0cab4a2cd
vincent-nephila <vincent@nephilaweb.com.ph>
2017-07-25 15:50:02 +0800

    Update 07/25/2017 Indexed -  change order of view

--- resources/views/cashier/index.blade.php
+++ resources/views/cashier/index.blade.php
@@ -10,13 +10,13 @@
     <div class="col-lg-12">
         <div id="searchbody">   
             <table class="table table-striped"><thead>
-            <tr><th>Student Number</th><th>Student Name</th><th>Gender</th><th>View</th><th>Info</th></tr>        
+            <tr><th>Student Number</th><th>Student Name</th><th>Gender</th><th>Info</th><th>View</th></tr>        
             </thead>
             <tbody>
                
             @foreach($students as $student)
             <tr><td>{{$student->idno}}</td><td>{{$student->lastname}}, {{$student->firstname}} {{$student->middlename}}
-                    {{$student->extensionname}}</td><td>{{$student->gender}}</td><td><a href = "{{url('/cashier',$student->idno)}}">view</a></td><td><a href = "{{url('/studentinfo',$student->idno)}}">view</a></td></tr>
+                    {{$student->extensionname}}</td><td>{{$student->gender}}</td><td><a href = "{{url('/studentinfo',$student->idno)}}">view</a></td><td><a href = "{{url('/cashier',$student->idno)}}">view</a></td></tr>
             @endforeach
             </tbody>
             </table>
